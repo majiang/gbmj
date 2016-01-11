313416db5bb705260e4716f03ca48779f9fb7d0f
majiang <neuron.ohori@gmail.com>
2016-01-11 22:07:20 +0900

    MeldType.kongSelf = 15 instead of 11

--- source/gbmj/meld.d
+++ source/gbmj/meld.d
@@ -7,8 +7,8 @@ import gbmj.internal : low, high;
 enum MeldType
 {
     pungRight = 4, pungOpposite, pungLeft,
-    kongRight = 8, kongOpposite, kongLeft, kongSelf,
-    pungRightKong = 12, pungOppositeKong, pungLeftKong,
+    kongRight = 8, kongOpposite, kongLeft,
+    pungRightKong = 12, pungOppositeKong, pungLeftKong, kongSelf,
     chowLow = 16, chowMiddle, chowHigh,
     flower = 32
 }
@@ -23,17 +23,17 @@ struct Meld
         import std.string : format;
         switch (meldType)
         {
-            case MeldType.pungRight: .. case MeldType.pungLeftKong: return [
+            case MeldType.pungRight: .. case MeldType.kongSelf: return [
                 MeldType.pungRight:"[%1$s%1$s[%1$s]]",
                 MeldType.pungOpposite:"[%1$s[%1$s]%1$s]",
                 MeldType.pungLeft:"[[%1$s]%1$s%1$s]",
                 MeldType.kongRight:"[%1$s%1$s%1$s[%1$s]]",
                 MeldType.kongOpposite:"[%1$s[%1$s]%1$s%1$s]",
                 MeldType.kongLeft:"[[%1$s]%1$s%1$s%1$s]",
-                MeldType.kongSelf:"[%1$s%1$s%1$s%1$s]",
                 MeldType.pungRightKong:"[%1$s%1$s[%1$s%1$s]]",
                 MeldType.pungOppositeKong:"[%1$s[%1$s%1$s]%1$s]",
-                MeldType.pungLeftKong:"[[%1$s%1$s]%1$s%1$s]"]
+                MeldType.pungLeftKong:"[[%1$s%1$s]%1$s%1$s]",
+                MeldType.kongSelf:"[%1$s%1$s%1$s%1$s]"]
             [meldType].format(tile.toString);
             case MeldType.chowLow: return "[[%s]%s%s]".format(tile.low, tile, tile.high);
             case MeldType.chowMiddle: return "[[%s]%s%s]".format(tile, tile.low, tile.high);
