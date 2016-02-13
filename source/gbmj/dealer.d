2e80ed64ad914d512621494f0bf5863f37521adb
majiang <neuron.ohori@gmail.com>
2016-02-13 22:40:47 +0900

    dealer: add in-conditions, document comments, explicit types

--- source/gbmj/dealer.d
+++ source/gbmj/dealer.d
@@ -1,10 +1,14 @@
 module gbmj.dealer;
 import gbmj.tile;
 
-///
+/** The wall.
+
+TODO:
+duplicate mode
+*/
 class Dealer
 {
-    ///
+    /// Shuffle and build the wall.
     this ()
     {
         this.tiles = allTiles;
@@ -14,20 +18,30 @@ class Dealer
             randomShuffle(tiles);
         }
     }
-    ///
+    /// Check if the wall is _empty.
     bool empty() @property
     {
         return tiles.length == 0;
     }
-    ///
-    auto deal()
+    /// Deal a Tile[] of length 13 for a player.
+    Tile[] deal()
+    in
+    {
+        assert (13 <= tiles.length);
+    }
+    body
     {
         auto ret = tiles[0..13];
         tiles = tiles[13..$];
         return ret;
     }
-    ///
-    auto pick()
+    /// Pick a Tile for a player.
+    Tile pick()
+    in
+    {
+        assert (!empty);
+    }
+    body
     {
         auto ret = tiles[0];
         tiles = tiles[1..$];
